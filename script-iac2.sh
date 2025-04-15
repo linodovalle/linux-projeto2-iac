1f5bb92ead8080483f1710376cef585282997ac1
linodovalle <linodovalle@hotmail.com>
2025-04-15 17:53:59 -0300

    Apache v.1.0 - Ajuste

--- script-iac2.sh
+++ script-iac2.sh
@@ -7,7 +7,6 @@ apt-get install apache2 -y
 apt-get install unzip -y
 
 echo "Baixando e copiando os arquivos da aplicação..."
-
 cd /tmp
 wget https://github.com/denilsonbonatti/linux-site-dio/archive/refs/heads/main.zip
 unzip main.zip
